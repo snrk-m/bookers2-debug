3143ea7b6726a910a626c764d223cf7ebbfa1e1c
EC2 Default User <ec2-user@ip-172-31-57-127.ec2.internal>
2021-04-06 08:43:25 +0000

    Add Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get "/home/about" => "homes#about"
-  resources :books
-  resources :users, only: [:show, :edit, :update, :index]
-
-  resources :book, only: [:new, :create, :index, :show, :destroy] do
+  resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show, :edit, :update, :index]
 end
